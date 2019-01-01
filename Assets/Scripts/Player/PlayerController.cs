cd38098e6eeda6b2f18f2f35ce8a92576d8d2a7e
X550ZE\David <davidtheoxcalibur@gmail.com>
2019-01-02 02:37:06 +0700

    Adjust controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,14 @@ public class PlayerController : MonoBehaviour
 {
 	[HideInInspector]
 	public bool facingRight = true;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
 	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+    public float horizontalDrag = 0.3f;
+
+    public float fallMultiplier = 2.5f;
+    public float releaseMultiplier = 2f;
+    public float airControlMultiplier = 0.5f;
 
 	private Transform groundCheck;			// A position marking where to check if the player is grounded.
 	private bool grounded = false;			// Whether or not the player is grounded.
@@ -17,52 +20,72 @@ public class PlayerController : MonoBehaviour
 
 	private Animator anim;					// Reference to the player's animator component.
 	private float crossHandTimer = 0f;
+    private Rigidbody2D rb;
+    private float defaultGravityScale;
      
 	void Awake()
 	{
 		// Setting up references.
 		anim = GetComponent<Animator>();
-	}
+        rb = GetComponent<Rigidbody2D>();
+        defaultGravityScale = rb.gravityScale;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+    }
 
-	void Update()
+    void FixedUpdate ()
 	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundChecks[0].position, 1 << LayerMask.NameToLayer("Ground"))
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        grounded = Physics2D.Linecast(transform.position, groundChecks[0].position, 1 << LayerMask.NameToLayer("Ground"))
                     || Physics2D.Linecast(transform.position, groundChecks[1].position, 1 << LayerMask.NameToLayer("Ground"));
+        // Cache the horizontal input.
+        float h = Input.GetAxis("Horizontal");
 
         anim.SetBool("Grounded", grounded);
-		anim.SetFloat ("VerSpeed", GetComponent<Rigidbody2D> ().velocity.y);
-		anim.SetFloat ("HorSpeed", Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x));
-		crossHandTimer += Time.deltaTime;
-		if (crossHandTimer > 5f) {
-			anim.SetTrigger ("Cross");
-			crossHandTimer = 0;
-		}
-		if (Input.anyKey) {
-			crossHandTimer = 0f;
-		}
+        anim.SetFloat("VerSpeed", rb.velocity.y);
+        anim.SetFloat("HorSpeed", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("Speed", Mathf.Abs(h));
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if((Input.GetButtonDown("Jump")) && grounded)
-			jump = true;
-	}
+        crossHandTimer += Time.fixedDeltaTime;
+        if (crossHandTimer > 5f)
+        {
+            anim.SetTrigger("Cross");
+            crossHandTimer = 0;
+        }
+        if (Input.anyKey)
+        {
+            crossHandTimer = 0f;
+        }
 
-
-	void FixedUpdate ()
-	{
-		// Cache the horizontal input.
-		float h = Input.GetAxis("Horizontal");
-
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+        if (grounded)
+        {
+            rb.gravityScale = defaultGravityScale;
+        }
+        if (rb.velocity.y > 0.3 && !Input.GetButton("Jump"))
+            rb.gravityScale = defaultGravityScale * releaseMultiplier;
+        if (rb.velocity.y < -0.3 && !grounded)
+            rb.gravityScale = defaultGravityScale * fallMultiplier;
 
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h !=0)
-			// ... add a force to the player.
-			GetComponent<Rigidbody2D>().velocity = new Vector2(h* maxSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		if(h != 0)
+        {
+            // ... add a force to the player.
+            if (grounded)
+                rb.AddForce(new Vector2(h * moveForce * Time.fixedDeltaTime, 0));
+            else
+                rb.AddForce(new Vector2(h * moveForce * airControlMultiplier * Time.fixedDeltaTime, 0));
 
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
+            if (rb.velocity.x > maxSpeed)
+                rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+            if (rb.velocity.x < maxSpeed*-1)
+                rb.velocity = new Vector2(maxSpeed*-1, rb.velocity.y);
+        }
+        else if (grounded && rb.velocity.x != 0)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x * -1 * horizontalDrag * Time.fixedDeltaTime, 0));
+        }
+
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
 			// ... flip the player.
 			Flip();
 		// Otherwise if the input is moving the player left and the player is facing right...
@@ -71,19 +94,14 @@ public class PlayerController : MonoBehaviour
 			Flip();
 
 		// If the player should jump...
-		if(jump)
+		if((Input.GetButtonDown("Jump")) && grounded)
 		{
 			// Set the Jump animator trigger parameter.
 			anim.SetTrigger("Jump");
 			//reset the y velocity
-
-			GetComponent<Rigidbody2D> ().velocity = new Vector2(GetComponent<Rigidbody2D> ().velocity.x, 0f) ;
-
+			rb.velocity = new Vector2(rb.velocity.x, 0f) ;
 			// Add a vertical force to the player.
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
+			rb.AddForce(new Vector2(0f, jumpForce));
 		}
 	}
 	
@@ -92,7 +110,6 @@ public class PlayerController : MonoBehaviour
 	{
 		// Switch the way the player is labelled as facing.
 		facingRight = !facingRight;
-
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
