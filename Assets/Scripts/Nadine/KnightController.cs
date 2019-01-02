a9b3d0d810806252529d9d42e5b76ef6777d2ca7
X550ZE\David <davidtheoxcalibur@gmail.com>
2019-01-02 07:40:58 +0700

    Add knight proto

---
+++ Assets/Scripts/Nadine/KnightController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KnightController : MonoBehaviour
+{
+    public GameObject knight;
+    public Sprite attackSprite;
+    public Sprite defendSprite;
+    private bool shield = false;
+    void Update()
+    {
+        if (Input.GetButtonDown("Knight"))
+        {
+            knight.SetActive(true);
+        }
+        if (Input.GetButtonUp("Knight"))
+        {
+            knight.SetActive(false);
+        }
+        if (Input.GetAxis("Shield") == 1 && !shield)
+        {
+            knight.GetComponent<SpriteRenderer>().sprite = defendSprite;
+            shield = true;
+        }
+        if (Input.GetAxis("Shield") == 0 && shield)
+        {
+            knight.GetComponent<SpriteRenderer>().sprite = attackSprite;
+            shield = false;
+        }
+    }
+}
