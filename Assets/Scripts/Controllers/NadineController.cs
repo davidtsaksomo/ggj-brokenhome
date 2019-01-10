668a07019152e9cbb277e0a7aff3a568acce9e97
agent-whisper <fariz.tumbuan@gmail.com>
2019-01-10 11:45:11 +0700

    Refactored some variables to separate scripts

---
+++ Assets/Scripts/Controllers/NadineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NadineController : Controller
+{
+    public override void Awake()
+    {}
+
+    public override void Start()
+    {}
+
+    public override void Update()
+    {}
+
+    public override void FixedUpdate()
+    {
+        horizontalAxisInput = Input.GetAxis("Horizontal");
+        verticalAxisInput = Input.GetAxis("Vertical");
+
+        if(horizontalAxisInput < 0.1f && horizontalAxisInput > -0.1f)
+        {
+            horizontalAxisInput = 0;
+        }
+    }
+}
