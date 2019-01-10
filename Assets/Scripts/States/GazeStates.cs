acf8cd287ab03a19d449319cf7f1bdfa7f3739f6
agent-whisper <fariz.tumbuan@gmail.com>
2019-01-10 11:45:55 +0700

    Created some states: Action, Gaze, Mobility, Position

---
+++ Assets/Scripts/States/GazeStates.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GazeStates 
+{
+    Unknown,
+    FacingRight,
+    FacingLeft,
+    Neutral,
+    LookingUp,
+    LookingDown
+}
---
+++ Assets/Scripts/States/PositionStates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PositionStates 
+{
+    Unknown,
+    Grounded,
+    OnAir,
+    OnLedge,
+    WaitingLeap
+}
