24eb1cb09ccf26d06110940ec08be32696e0c388
agent-whisper <fariz.tumbuan@gmail.com>
2019-01-10 11:41:56 +0700

    Implemented player collider script

---
+++ Assets/Scripts/ActorColliders/PlayerCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActorCollider2D : MonoBehaviour
+{
+    public Actor actor;
+
+    void Awake()
+    {
+        CapsuleCollider2D collider = gameObject.AddComponent(typeof(CapsuleCollider2D)) as CapsuleCollider2D;
+        actor = GetComponent<Actor>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy" || (coll.gameObject.tag == "Harmful"))
+        {
+            actor.Respawn();
+        }
+    }
+}
