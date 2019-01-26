f71e78cd7e7d5e9c30a56126cd446d0ea8e49f78
X550ZE\David <davidtheoxcalibur@gmail.com>
2019-01-26 16:23:42 +0700

    Add sky color

--- Assets/Scripts/SkyController.cs
+++ Assets/Scripts/SkyController.cs
@@ -1,20 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkyController : MonoBehaviour
 {
     public GameObject[] images;
-    int currentIndex;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform player;
+    public float cycleSteps;
+    int currentIndex = 0;
+    int currentStep = 1;
+    Color currentColor;
     void Update()
     {
-        
+        if(player.position.x > currentStep * cycleSteps)
+        {
+            ChangeSkyIndex();
+            currentStep += 1;
+            print("a");
+        }
+    }
+
+    void ChangeSkyIndex()
+    {
+        foreach(GameObject image in images)
+        {
+            image.SetActive(false);
+        }
+        int newIndex = (currentIndex + 1) % images.Length;
+        if(newIndex != 0)
+            images[currentIndex].SetActive(true);
+        images[newIndex].SetActive(true);
+        currentColor = images[newIndex].GetComponent<Image>().color;
+        images[newIndex].GetComponent<Image>().color = new Color(currentColor.r, currentColor.g, currentColor.b, 0f);
+        StartCoroutine(FadeIn(newIndex));
+
+    }
+
+    IEnumerator FadeIn(int newIndex)
+    {
+        float alpha = 0;
+        while(alpha < 1f)
+        {
+            alpha += 0.025f;
+            images[newIndex].GetComponent<Image>().color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+            yield return null;
+        }
+        images[currentIndex].SetActive(false);
+        currentIndex = newIndex;
     }
 }
